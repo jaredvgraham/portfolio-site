e2bd718fa5b04bfa8901dc5d2c6c2ff6433943f1
JaredvGraham <jgram153@icloud.com>
2024-08-03 20:37:34 -0400

    Save changes before filtering history

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAySU6_moSVzRbBMUAX7BcITrFgUKvOfJc",
-  authDomain: "portfolio-47b3f.firebaseapp.com",
-  projectId: "portfolio-47b3f",
-  storageBucket: "portfolio-47b3f.appspot.com",
-  messagingSenderId: "924140165720",
-  appId: "1:924140165720:web:23493a10578674e7f2c82d",
-  measurementId: "G-RDBNB3Q3XS",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
